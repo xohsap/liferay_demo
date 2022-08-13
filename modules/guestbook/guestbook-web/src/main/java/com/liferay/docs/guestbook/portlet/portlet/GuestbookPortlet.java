51a5da5ac6857d1680724826894263b7f5111641
PavelBogdanov <xohsap@gmail.com>
2022-08-14 00:26:45 +0300

    Defining the Component Metadata Properties Refactoring the Prototype Step 2 of 6

--- modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
+++ modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
@@ -20,15 +20,16 @@ import java.util.logging.Logger;
 @Component(
     immediate = true,
     property = {
-        "com.liferay.portlet.display-category=category.sample",
-        "com.liferay.portlet.header-portlet-css=/css/main.css",
-        "com.liferay.portlet.instanceable=true",
+        "com.liferay.portlet.display-category=category.social",
+        "com.liferay.portlet.instanceable=false",
+        "com.liferay.portlet.scopeable=true",
         "javax.portlet.display-name=Guestbook",
+        "javax.portlet.expiration-cache=0",
         "javax.portlet.init-param.template-path=/",
         "javax.portlet.init-param.view-template=/view.jsp",
-        "javax.portlet.name=" + GuestbookPortletKeys.GUESTBOOK,
         "javax.portlet.resource-bundle=content.Language",
-        "javax.portlet.security-role-ref=power-user,user"
+        "javax.portlet.security-role-ref=power-user,user",
+        "javax.portlet.supports.mime-type=text/html"
     },
     service = Portlet.class
 )
