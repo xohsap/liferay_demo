f883c769a7b73b8ba0bc5381dca8acee809de486
PavelBogdanov <xohsap@gmail.com>
2022-08-13 22:43:18 +0300

    Writing Your First Liferay DXP Application

---
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
@@ -0,0 +1,30 @@
+package com.liferay.docs.guestbook.portlet.portlet;
+
+import com.liferay.docs.guestbook.portlet.constants.GuestbookPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author xohsa
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Guestbook",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + GuestbookPortletKeys.GUESTBOOK,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class GuestbookPortlet extends MVCPortlet {
+}
