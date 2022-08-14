2468b3ef61ee8454fdb782e31930735fc2c4d333
PavelBogdanov <xohsap@gmail.com>
2022-08-14 21:09:36 +0300

    Adding Metadata Writing the Guestbook Admin App Step 2 of 5

--- modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookAdminPortlet.java
+++ modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookAdminPortlet.java
@@ -1,6 +1,28 @@
 package com.liferay.docs.guestbook.portlet.portlet;
 
+import com.liferay.docs.guestbook.portlet.constants.GuestbookPortletKeys;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import org.osgi.service.component.annotations.Component;
 
+import javax.portlet.Portlet;
+
+@Component(
+    immediate = true,
+    property = {
+        "com.liferay.portlet.display-category=category.hidden",
+        "com.liferay.portlet.scopeable=true",
+        "javax.portlet.display-name=Guestbooks",
+        "javax.portlet.expiration-cache=0",
+        "javax.portlet.init-param.portlet-title-based-navigation=true",
+        "javax.portlet.init-param.template-path=/",
+        "javax.portlet.init-param.view-template=/view.jsp",
+        "javax.portlet.name=" + GuestbookPortletKeys.GUESTBOOK_ADMIN,
+        "javax.portlet.resource-bundle=content.Language",
+        "javax.portlet.security-role-ref=administrator",
+        "javax.portlet.supports.mime-type=text/html",
+        "com.liferay.portlet.add-default-resource=true"
+    },
+    service = Portlet.class
+)
 public class GuestbookAdminPortlet extends MVCPortlet {
 }
