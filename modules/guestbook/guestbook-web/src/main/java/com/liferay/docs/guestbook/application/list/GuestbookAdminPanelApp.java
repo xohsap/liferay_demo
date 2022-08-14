73c83cac043a6fc33d57e8d0c81b69eecf42d161
PavelBogdanov <xohsap@gmail.com>
2022-08-14 15:19:18 +0300

    Creating the Classes Writing the Guestbook Admin App Step 1 of 5

---
+++ modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/application/list/GuestbookAdminPanelApp.java
@@ -0,0 +1,10 @@
+package com.liferay.docs.guestbook.application.list;
+
+import com.liferay.application.list.BasePanelApp;
+
+public class GuestbookAdminPanelApp extends BasePanelApp {
+    @Override
+    public String getPortletId() {
+        return null;
+    }
+}
---
+++ modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookAdminPortlet.java
@@ -0,0 +1,6 @@
+package com.liferay.docs.guestbook.portlet.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+public class GuestbookAdminPortlet extends MVCPortlet {
+}
