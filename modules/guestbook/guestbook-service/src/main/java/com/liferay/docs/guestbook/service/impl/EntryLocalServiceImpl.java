015f6ea2ff13b2b0e09c052e4cf2ca1808902632
PavelBogdanov <xohsap@gmail.com>
2022-08-15 20:55:09 +0300

    Handling Indexing in the Entry Service Layer Enabling Search and Indexing for Entries Step 5 of 5

--- modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/EntryLocalServiceImpl.java
+++ modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/EntryLocalServiceImpl.java
@@ -24,6 +24,8 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
@@ -41,6 +43,7 @@ import java.util.List;
 )
 public class EntryLocalServiceImpl extends EntryLocalServiceBaseImpl {
 
+    @Indexable(type = IndexableType.REINDEX)
     public Entry addEntry(
         long userId, long guestbookId, String name, String email,
         String message, ServiceContext serviceContext)
@@ -79,6 +82,7 @@ public class EntryLocalServiceImpl extends EntryLocalServiceBaseImpl {
         return entry;
     }
 
+    @Indexable(type = IndexableType.REINDEX)
     public Entry updateEntry(
         long userId, long guestbookId, long entryId, String name, String email,
         String message, ServiceContext serviceContext)
@@ -110,6 +114,7 @@ public class EntryLocalServiceImpl extends EntryLocalServiceBaseImpl {
         return entry;
     }
 
+    @Indexable(type = IndexableType.DELETE)
     public Entry deleteEntry(long entryId, ServiceContext serviceContext)
         throws PortalException {
 
